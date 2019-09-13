e61a4c01a4d25fe021b8be5f630fa62d2ae10ab0
Isyaku Murtala <muriisyaku68@gmail.com>
2019-09-13 12:45:42 +0100

    PythonGmail

---
+++ PythonGmail.py
@@ -0,0 +1,32 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
+sender_email = input("Please enter your Email Address: \n")
+receiver_email = input("Please enter reciever Email Address: \n")
+password = input(str("Please enter your Email Password: \n"))
+subject = input("Please enter Email Subject: \n")
+
+message = MIMEMultipart()
+message['From'] = sender_email
+message['To'] = receiver_email
+message['Subject'] = subject
+
+
+body = input("Typr your Message: ")
+message.attach(MIMEText(body,'plain'))
+text = message.as_string()
+
+mail = smtplib.SMTP('smtp.gmail.com', 587)
+mail.starttls()
+mail.login(sender_email, password)
+print("Login Successfuly")
+
+mail.sendmail(sender_email, receiver_email, text)
+print("Email Sent to", receiver_email)
+mail.quit()
+
+
+
+# 1234567890python
+# pythonwork61@gmail.com
